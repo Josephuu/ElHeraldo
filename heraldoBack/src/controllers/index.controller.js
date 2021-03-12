5cad70e7a8195714c47174cbf3960bfbf21d9da9
Pr0vius <braiandelgado4255@hotmail.com>
2021-03-12 18:47:57 -0300

    Actualizando el back

--- heraldoBack/src/controllers/index.controller.js
+++ heraldoBack/src/controllers/index.controller.js
@@ -1,7 +1,39 @@
 const indexCtrl = {};
+const podcast = require('../models/podcast')
+const article = require('../models/news.schema')
 
-indexCtrl.renderIndex= (req,res)=>{
-    res.render('index')
+indexCtrl.renderIndex= async(req,res)=>{
+    const page = parseInt(req.query.page, 10) || 1;
+
+    var prevPage = parseInt(req.query.prevPage, 10);
+    if(prevPage < 0 ){prevPage = 1};
+    const firstPage = 1;
+    var totalDocs= req.query.totalDocs;
+    const limit = 3;
+    const totalPages = req.query.totalPages;
+    var nextPage = req.query.nextPage;
+    var prevPage = req.query.prevPage ;
+    const sort = {createdAt:-1}
+
+    const PODCAST = await podcast.paginate({},{
+        limit,
+        page,
+        prevPage,
+        nextPage,
+        totalPages,
+        sort});
+    const podcastDoc = PODCAST.docs
+
+    const ARTICLE = await article.paginate({},{
+        limit,
+        page,
+        prevPage,
+        nextPage,
+        totalPages,
+        sort});
+    const articleDoc = ARTICLE.docs
+
+    res.render('index',[{podcastDoc,PODCAST},{articleDoc,ARTICLE}])
 };
 
 module.exports = indexCtrl;
--- heraldoBack/src/routes/main.routes.js
+++ heraldoBack/src/routes/main.routes.js
@@ -5,6 +5,7 @@ const { renderIndex } = require('../controllers/index.controller');
 const { renderNoticia } = require('../controllers/new.controller');
 const { renderNoticias } = require('../controllers/news.controller');
 const { renderProgramacion } = require('../controllers/programation.controller');
+// const { renderPodcasts } = require('../controllers/podcast.controller');
 
 router.get('/', renderIndex);
 
@@ -12,7 +13,9 @@ router.get('/noticia-especifica',renderNoticia);
 
 router.get('/noticias', renderNoticias);
 
-router.get('/programas', renderProgramacion)
+router.get('/programas', renderProgramacion);
+
+//router.get('/podcast', renderPodcasts);
 
 
 module.exports = router;
