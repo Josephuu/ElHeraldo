9812e531c7504ab9fd7946ea71ed2544c2365f30
Pr0vius <braiandelgado4255@hotmail.com>
2021-03-12 19:28:52 -0300

    meti cosas en el front y se rompio todo xd

--- heraldoBack/src/controllers/index.controller.js
+++ heraldoBack/src/controllers/index.controller.js
@@ -6,13 +6,13 @@ indexCtrl.renderIndex= async(req,res)=>{
     const page = parseInt(req.query.page, 10) || 1;
 
     var prevPage = parseInt(req.query.prevPage, 10);
-    if(prevPage < 0 ){prevPage = 1};
     const firstPage = 1;
     var totalDocs= req.query.totalDocs;
     const limit = 3;
     const totalPages = req.query.totalPages;
     var nextPage = req.query.nextPage;
     var prevPage = req.query.prevPage ;
+    if(prevPage <= 0 ){prevPage = 1};
     const sort = {createdAt:-1}
 
     const PODCAST = await podcast.paginate({},{
